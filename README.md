67385fb95547cbeab80a8d7e459c930808e79413
Aswin Radhakrishnan <aswin.radhakrishnan@cba.com.au>
2016-01-25 14:25:26 +1100

    adding chronos dashboard to ocular

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ocular
+Introduction
+============
+
+**Ocular Dashboard** 
+
+Chronos Dashboard
+------------------------------
+**Displays all the chronos jobs running. It helps filtering Jobs based on their run status, shows the job details, provides ability to add/edit/delete/force start the jobs **
---
+++ plugins/util/queryChronos.js
@@ -0,0 +1,26 @@
+function getJobList($scope, $http) {
+    $http.get('http://localhost:4400/scheduler/jobs').
+        success(function(data) {
+            $scope.jobList = data;
+        });
+}
+
+function getChronosCount($scope, $http) {
+    $http.get('http://localhost:4400/scheduler/jobs').success(function(data) {
+        var successCount = 0;
+        var failureCount = 0;
+        var idleCount = 0;
+        for(var i=0; i < data.length; i++){
+            if(data[i].lastSuccess > data[i].lastError)
+                successCount += 1
+            else if(data[i].lastSuccess < data[i].lastError)
+                failureCount += 1
+            else 
+                idleCount += 1
+       }
+        $scope.successCount = successCount;
+        $scope.numberOfJobs = data.length;
+        $scope.failureCount = failureCount
+        $scope.idleCount= idleCount
+        });
+}
